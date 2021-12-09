987cc249737472d21ee27bbd6b68ba717f9e02ae
JideOgun <JideOgun@github.com>
2021-12-09 10:45:06 -0600

    updates

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,10 @@ var carddiv = document.getElementById('card-div-id');
 
 
 
-// Forloop for persisting the data onto HMTL page
+// // search history persistence - getting the data
 for (var i = 0; i < localStorage.length; i++) {
 
     var city = localStorage.getItem(i);
-    // console.log(localStorage.getItem("City"));
     var cityName = document.getElementById('city-list-id')
  var newlist = document.createElement('button');
  newlist.textContent = city;
@@ -23,15 +22,9 @@ for (var i = 0; i < localStorage.length; i++) {
 // Key count for local storage 
 var key = 0;
 
-
-
-
-
-
+// Retreiving the open weather API url and using json() to parse it
 var getApi = function() {
-    var searchFormInput = document.getElementById('form-input').value
-
-console.log(searchFormInput)
+    var searchFormInput = document.getElementById('form-input').value;
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+searchFormInput+'&appid=640fe4f6b319bdea19e40e5ba9e99f7e';
   
     fetch(requestUrl)
@@ -45,7 +38,7 @@ console.log(searchFormInput)
         var Lat = data.coord.lat;
 
     
-// search history persistence
+// search history persistence - setting the data
 var city = document.getElementById('city-list-id');
 var btnEl = document.createElement('button');
 btnEl.textContent = data.name;
@@ -56,93 +49,95 @@ var localStorageVar = localStorage.setItem(key, data.name);
 key = key + 1;
 
 
+// stops elements from appending to container continuously when the search button is clicked by first emptying whatever is in there and generating new data
+$('#stats-div').empty();
+// create a new div, set the textcontext to data.name and append to statsdiv
+var cityName = document.createElement('h2');
 
-        $('#stats-div').empty();
-        // create a new div, set the textcontext to data.name and append to statsdiv
-        var cityName = document.createElement('h2');
-        
-            cityName.textContent = `${currentHour} `+data.name;
-            statsdiv.append(cityName);
-
-
-                    // create a new div, set the textcontext to data.main.temp and append to statsdiv
-        var tempValue = document.createElement('h5');
-        tempValue.textContent = 'temp: '+data.main.temp+'K';
-        statsdiv.append(tempValue);
-
-        // create a new div, set the textcontext to data.name and append to statsdiv
-        var humValue = document.createElement('h5');
-        humValue.textContent = 'Humidity: '+data.main.humidity+'%';
-        statsdiv.append(humValue);
-
-   // create a new div, set the textcontext to data.name and append to statsdiv
-   var windValue = document.createElement('h5');
-   windValue.textContent = 'Wind Speed: '+data.wind.speed+'mph';
-   statsdiv.append(windValue);
+cityName.textContent = `${currentHour} `+data.name;
+statsdiv.append(cityName);
 
 
+// create a new div, set the textcontext to data.main.temp and append to statsdiv
+var tempValue = document.createElement('h5');
+tempValue.textContent = 'temp: '+data.main.temp+'K';
+statsdiv.append(tempValue);
 
-        var url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+Lat+'&'+'lon='+Lon+'&exclude=hourly,daily&appid=640fe4f6b319bdea19e40e5ba9e99f7e';
-        fetch(url)
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(dat)
-        {console.log(dat)
-        // create a new div, set the textcontext to data.name and append to statsdiv
-    var uvValue = document.createElement('div');
-    uvValue.textContent = 'UV Index: '+dat.current.uvi;
-    statsdiv.appendChild(uvValue);
-        })
+// create a new div, set the textcontext to data.name and append to statsdiv
+var humValue = document.createElement('h5');
+humValue.textContent = 'Humidity: '+data.main.humidity+'%';
+statsdiv.append(humValue);
+
+// create a new div, set the textcontext to data.name and append to statsdiv
+var windValue = document.createElement('h5');
+windValue.textContent = 'Wind Speed: '+data.wind.speed+'mph';
+statsdiv.append(windValue);
 
 
 
-
-        var url_link = 'https://api.openweathermap.org/data/2.5/forecast?q='+data.name+'&appid=640fe4f6b319bdea19e40e5ba9e99f7e';
-        fetch(url_link)
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function(forecastdata)
-        {
-            console.log(forecastdata);
-
-            $('#card-div-id').empty();
-        // create a new div, set the textcontext to data.name and append to card-div
-
-        for(var i = 0; i < 5; i++) {
-    var forecastInfo = document.createElement('card');
-    forecastInfo.textContent = forecastdata.list[i].dt_txt;
-    carddiv.appendChild(forecastInfo);
-    var icon = forecastdata.list[i].weather[0].icon;
-    console.log(icon);
-    var img = document.createElement("img");
-    img.src =  `http://openweathermap.org/img/wn/${icon}@2x.png`
-    
-    forecastInfo.append(img);
-
-
-var tempVal = document.createElement('p');
-tempVal.textContent = 'Temp: '+forecastdata.list[i].main.temp+'K';
-forecastInfo.appendChild(tempVal);
-var windVal = document.createElement('p');
-windVal.textContent = 'Wind: '+forecastdata.list[i].wind.speed+' mph';
-forecastInfo.appendChild(windVal);
-var humVal = document.createElement('p');
-humVal.textContent = 'Humidity: '+forecastdata.list[i].main.humidity+'%';
-forecastInfo.appendChild(humVal);
-        }
-
+var url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+Lat+'&'+'lon='+Lon+'&exclude=hourly,daily&appid=640fe4f6b319bdea19e40e5ba9e99f7e';
+fetch(url)
+.then(function(res) {
+    return res.json();
+})
+.then(function(dat)
+{console.log(dat)
+// create a new div, set the textcontext to data.name and append to statsdiv
+var uvValue = document.createElement('div');
+uvValue.textContent = 'UV Index: '+dat.current.uvi;
+statsdiv.appendChild(uvValue);
     })
 
 
 
 
-
+var url_link = 'https://api.openweathermap.org/data/2.5/forecast?q='+data.name+'&appid=640fe4f6b319bdea19e40e5ba9e99f7e';
+fetch(url_link)
+.then(function(resp) {
+    return resp.json();
 })
+.then(function(forecastdata)
+{
+    console.log(forecastdata);
+
+    $('#card-div-id').empty();
 
 
+    // creating an array of days I want to loop over because the api give trhe forecast in 3 hour increments so using a regular for loop will return the wrong data
+var day = [0, 8, 16, 24, 32];
+day.forEach(function(i) {
+    // create a new div, set the textcontext to data.name and append to card-div
+    var forecastInfo = document.createElement('card');
+    forecastInfo.className = "newcard";
+    forecastInfo.textContent = forecastdata.list[i].dt_txt;
+    carddiv.appendChild(forecastInfo);
 
+    // creating the weather icon element and appending it to the forecast container
+    var icon = forecastdata.list[i].weather[0].icon;
+    console.log(icon);
+    var img = document.createElement("img");
+    img.src =  `http://openweathermap.org/img/wn/${icon}@2x.png`
+    forecastInfo.append(img);
+
+    // creating the temperature element and appending it to the forecast container
+    var tempVal = document.createElement('p');
+    tempVal.className = "newcard";
+    tempVal.textContent = 'Temp: '+forecastdata.list[i].main.temp+'K';
+    forecastInfo.appendChild(tempVal);
+
+    // creating the wind speed element and appending it to the forecast container
+    var windVal = document.createElement('p');
+    windVal.className = "newcard";
+    windVal.textContent = 'Wind: '+forecastdata.list[i].wind.speed+' mph';
+    forecastInfo.appendChild(windVal);
+
+    // creating the humidity element and appending it to the forecast container
+    var humVal = document.createElement('p');
+    humVal.className = "newcard";
+    humVal.textContent = 'Humidity: '+forecastdata.list[i].main.humidity+'%';
+    forecastInfo.appendChild(humVal);
+        });
+    });
+});
 }
-
 searchbtn.addEventListener('click', getApi);
